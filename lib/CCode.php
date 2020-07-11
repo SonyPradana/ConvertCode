be444f0d4aae206982271d950ba91164514791ff
Angger Pradana <sonypradana@gmail.com>
2020-07-11 22:39:34 +0700

    initial commit

---
+++ lib/CCode.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Simple script to convert from number to code/id/string 
+ * 
+ * @version 1.0
+ * @author Angger Pradana
+ * @link https://github.com/SonyPradana/Ccode
+ */
+class CCode{
+    /** @var int Number for this section */
+    private $_number = 0;
+
+    /** @var array Array of code */
+    public $codes = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e", 
+        "f", 
+        "g", 
+        "h", 
+        "i", 
+        "j", 
+        "k", 
+        "l", 
+        "m", 
+        "n", 
+        "o", 
+        "p", 
+        "q", 
+        "r", 
+        "s", 
+        "t", 
+        "u", 
+        "v", 
+        "w", 
+        "x", 
+        "y", 
+        "z",
+        "A", 
+        "B", 
+        "C", 
+        "D", 
+        "E", 
+        "F", 
+        "G", 
+        "H", 
+        "I", 
+        "J", 
+        "K", 
+        "L", 
+        "M", 
+        "N", 
+        "O", 
+        "P", 
+        "Q", 
+        "R", 
+        "S", 
+        "T", 
+        "U", 
+        "V", 
+        "W", 
+        "X", 
+        "Y", 
+        "Z"
+    ];
+
+    /** Get code from this section
+     * @return string Code/id/string     * 
+     */
+    public function getCode():string{
+        $res = new CCode();
+        return $res->convertNumberToCode($this->_number);
+    }
+    /** Get number from this section
+     * @return int Number
+     */
+    public function getNumber():int{
+        return $this->_number;;
+    }
+    /** Set code/id to this section
+     * @param string $code Code/id/string to convert
+     */
+    public function setCode(string $code){
+        $res = new CCode();
+        $this->_number = $res->convertCodeToNumber($code);
+    }
+    /** Set number to this section
+     * @param int $number Number to convert
+     */
+    public function setNumber(int $number){
+        $this->_number = $number;
+    }
+
+    /** Add a certain number to this section 
+     * @param $number How many number (+) operator
+     * @return bool True if success execution
+     */
+    public function plusOperator(int $number = 1){
+        if( is_numeric($number) ){
+            $this->_number + $number;
+            return true;
+        }
+        return false;
+    }
+    /** Reduce certain numbers to this section 
+     * @param $number How many number (+) operator
+     * @return bool True if success execution*/
+    public function minusOperator(int $number = 1){
+        if( is_numeric($number) ){
+            $this->_number - $number;
+            return true;
+        }
+        return false;
+    }
+
+    // private methode
+    /** Convert from code to number
+     * @param string $code Code/id to convert
+     * @return int Result convert as number
+     */
+    private function convertCodeToNumber(string $code):int{
+        $arr_code = preg_split('//u', $code, -1, PREG_SPLIT_NO_EMPTY);
+        $digit = (int) count( $arr_code );
+        // One digit, return as array index from codes
+        if( $digit == 1){
+            return array_search(max($arr_code), $this->codes);
+        }
+        $sum = (int) -1;
+        $j = $digit -1;
+        for ($i = 0; $i < $digit; $i++) { 
+            $pos = array_search($arr_code[$i], $this->codes);
+            // var_dump( $digit );
+            $pw  = 62 ** $j;
+            $row = ($pw * $pos) - ($pw * $i) + 1;
+            $sum += $row; 
+            $j--;
+        }
+        $digit += 1;
+        return $sum + (62 * ($digit - 1)) - 62;
+    }
+    /** Convert from number to code
+     * @param int $number Number to convert
+     * @return string Result convert as code/id
+     */
+    private function convertNumberToCode(int $number):string{
+        // Negative number not allow
+        if( $number < 0 ) return false;
+        $code = []; 
+
+        if( $number < 62){
+            $code[] = $this->codes[$number];
+            return implode("", $code);
+        }
+
+        $num  = $number - 62; 
+
+        $digit = 0;
+        $max = 0;
+        while ($max <= $number) {
+            $max = pow(62, $digit);
+            $digit++;
+        }
+        for ($i = $digit; $i > 1; $i--) {
+            $pw = pow(62, $i-1);
+            $idx = (float) ($num / $pw);
+            $idx = floor( $idx );
+            $code[] = $this->codes[$idx] ;
+            $num = $num % $pw;
+        }
+
+        $code[] = $this->codes[$num];
+        // Remove 0 (zero) before "Z"
+        // if 0 number before Z exis couse false indexing
+        unset( $code[0] );
+        if( $code[1] == 0 && $code[2] == "Z" && isset( $code[3]) ){
+            unset( $code[1] );
+        }
+        $res = implode("", $code);
+        $res = $res == "" ? 0 : $res;
+        return (string) $res;  
+    }
+
+    // static method
+    /** Convert from code to number
+     * @param string $code Code/id to convert
+     * @return int Result convert as number
+     */
+    public static function ConvertFromCode(string $code):int{
+        $res = new CCode();
+        return $res->convertCodeToNumber($code);
+    }
+    /** Convert from number to code
+     * @param int $number Number to convert
+     * @return string Result convert as code/id
+     */
+    public static function ConvertToCode(int $number):string{
+        $res = new CCode();
+        return $res->convertNumberToCode($number);
+    }
+    /** Random code based on the specified numbers
+     * @param int $digit Makimum digit allowed
+     */
+    public static function RandomCode(int $digit = 4):string{
+        $digit = $digit < 0 ? 1 : $digit;
+        $ccode = new CCode();
+        $code = [];
+        for ($i=0; $i < $digit; $i++) { 
+            $code[] = $ccode->codes[rand(0, 61)];
+        }
+        return  implode("", $code);;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,87 @@
+# CCode
+Simple script to creat uniq id from number, support until up to **11.694.146.092.834.141** digit number. With suport convert back to id (string).
+
+We recommended if you need shorter id from long number ex: timestamp, id with large integer, or name think.
+
+Basic convert is creat id from array of string, then build from array index one by one.
+You can costumize by edit entry of array.
+
+By default array using [0-9][a-z][A-Z] its mean case sensitive. different between AA and aA
+
+### Sample
+```
+    94 = 0w
+    95 = 0x
+    96 = 0y
+    ...
+    3842 = YY
+    3843 = YZ
+    3844 = Z0
+    ...
+    238323 = ZYV
+    238324 = ZYW
+    238325 = ZYX
+```
+
+## Installation
+
+You can directly import this class from your project.
+```php
+include 'path_to_CCode_file' . '/CCode.php';
+```
+If using [Composer](https://getcomposer.org/)
+```bash
+composer require sonypradana/ccode;
+```
+
+## Usage
+You can make id using declare class or directly.
+
+### Using class:
+
+Craet class
+```php
+$ccode = new CCode();
+```
+Creat id (string) from number
+```php
+$ccode->setNumber( 999999 );  // creat id by 99
+echo $ccode->getCode();       // will out put -> 4c81
+```
+Creat id (string) from id/code/string
+```php
+$ccode->setCode( '4c82' );  // creat id by code "4c82"
+echo $ccode->getNumber();   // will out put -> 1000000
+```
+You can also using operator
+```php
+// plus (+) operator
+$ccode->plusOperator(1);    // plus 1
+echo $ccode->getNumber();   // will out put -> 1000001
+echo $ccode->getCode();     // will out put -> ac83
+
+// minus (-) operator
+$ccode->minusOperator(1);    // minus 2
+echo $ccode->getNumber();   // will out put -> 999999
+echo $ccode->getCode();     // will out put -> ac81
+```
+
+### Directly
+You also can call function directly
+
+Convert from **id/code/string** to **number**
+```php
+$id_number = CCode::ConvertFromCode( "0B" );
+echo $id_number; // will out put 99
+```
+Convert from **number** to **id/code/string**
+```php
+$id_code = CCode::ConvertFromNumber( 99 );
+echo $id_code; // will out put "0B"
+```
+
+If you want to get random **id/code/string**, you can use
+```php
+$id_code = CCode::RandomCode( 4 ); // how many digit code you want
+echo $id_code; // random code 
+```
